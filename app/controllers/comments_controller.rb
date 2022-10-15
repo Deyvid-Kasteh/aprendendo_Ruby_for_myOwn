50fd44904a67e2c3e8f537920b1f6947627f7a09
Deyvid Kasteh <99744584+Deyvid-Kasteh@users.noreply.github.com>
2022-10-15 03:28:04 -0300

    correção de erros no codigo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-    http-basic_authenticate-with name: "dhh", password: "secret", only: :destroy
+    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
